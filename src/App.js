a6a955f898164c102bb4c1aaf04b29f9f18c736f
Crystal <crystal123@hotmail.com>
2023-04-25 16:08:15 +1200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { AppContext } from './context/contextApi'
 import Header from './components/Header'
 import Feed from './components/Feed'
@@ -10,7 +10,6 @@ import VideoDetails from './components/VideoDetails'
 const App = () => {
   return (
   <AppContext>
-    <BrowserRouter>
       <div className='flex flex-col h-full'>
         <Header />
         <Routes>
@@ -19,7 +18,6 @@ const App = () => {
           <Route path="/video/:id" element={<VideoDetails />}/>
         </Routes>
         </div>
-    </BrowserRouter>
       </AppContext>
   )
 }
